f21242a2f28c4a2e2eb142a56246f815af38a82f
Kyle Derosha <kpderosh@gmail.com>
2025-01-15 22:59:09 -0500

    Updating some of the configuration used for the led strip

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
  *
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
+#include "driver/gpio.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
@@ -11,8 +12,9 @@
 
 // GPIO assignment
 #define LED_STRIP_GPIO_PIN 13
+#define MOTION_SENSOR_INPUT_PIN 27
 // Numbers of the LED in the strip
-#define LED_STRIP_LED_COUNT 300
+#define LED_STRIP_LED_COUNT 400
 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
 #define LED_STRIP_RMT_RES_HZ (10 * 1000 * 1000)
 
@@ -21,13 +23,11 @@ static const char *TAG = "example";
 led_strip_handle_t configure_led(void) {
   // LED strip general initialization, according to your led board design
   led_strip_config_t strip_config = {
-      .strip_gpio_num = LED_STRIP_GPIO_PIN, // The GPIO that connected to the
-                                            // LED strip's data line
-      .max_leds = LED_STRIP_LED_COUNT,      // The number of LEDs in the strip,
-      .led_model = LED_MODEL_WS2812,        // LED strip model
+      .strip_gpio_num = LED_STRIP_GPIO_PIN,
+      .max_leds = LED_STRIP_LED_COUNT,
+      .led_model = LED_MODEL_WS2812,
       .color_component_format =
           LED_STRIP_COLOR_COMPONENT_FMT_GRB, // The color order of the strip:
-                                             // GRB
       .flags = {
           .invert_out = false, // don't invert the output signal
       }};
@@ -52,22 +52,27 @@ led_strip_handle_t configure_led(void) {
   return led_strip;
 }
 
+int scaleBrightness(int colorToScale) {
+  double scaleFactor = .5;
+  return colorToScale * scaleFactor;
+}
+
 void app_main(void) {
   led_strip_handle_t led_strip = configure_led();
   int counter = 0;
   while (1) {
-    if (counter > 5) {
+    if (counter > 3) {
       counter = 0;
     }
     counter++;
 
     for (int i = 0; i < LED_STRIP_LED_COUNT; i++) {
-      if (i + counter % 3 == 0) {
-        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 0, 0, 255));
-      } else if (i + counter % 2 == 0) {
-        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 0, 255, 0));
+      if ((i + counter) % 3 == 0) {
+        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 0, 0, 100));
+      } else if ((i + counter) % 2 == 0) {
+        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 0, 100, 0));
       } else {
-        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 255, 0, 0));
+        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 100, 0, 0));
       }
     }
     ESP_ERROR_CHECK(led_strip_refresh(led_strip));
